d34f2bd5b8d1382e496237bde334ebdcc73f42d4
MoiseHarari <mharari3@babson.edu>
2021-02-17 17:36:42 -0500

    Worked on codingbat

---
+++ warmup-1/diff21.py
@@ -0,0 +1,13 @@
+def diff21(n):
+    """Given an int n, return the absolute difference
+     between n and 21, except return double the absolute 
+     difference if n is over 21."""
+    diff = 21 - n
+    if n < 21:
+        return abs(diff)
+    else:
+        return abs(diff)*2
+
+print(diff21(19))
+print(diff21(10))
+print(diff21(21))
